f13c56bae4a2168dd49cb8dc2bc8ddc2e4d16fac
BlastChaos <brutusjason123@gmail.com>
2023-10-13 23:04:20 -0400

    Add a SoftmaxPercents

--- AI Chess/GameConfig.cs
+++ AI Chess/GameConfig.cs
@@ -4,5 +4,6 @@ public class GameConfig
 {
     public string[] Users { get; set; }
     public string GamesOutputDirectory { get; set; }
-
+    public int NumberData { get; set; }
+    public double SoftmaxPercents { get; set; }
 }
