d695fd6ea0525cdde3d8b7dbc639c7b8e6dc428e
BlastChaos <76956740+BlastChaos@users.noreply.github.com>
2024-01-11 15:05:42 +0000

    use startup

--- AI Chess/Program.cs
+++ AI Chess/Program.cs
@@ -2,96 +2,21 @@ using AI_Chess;
 using AI_Chess.Activation;
 using AI_Chess.Context;
 using AI_Chess.Controllers;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddScoped<ChessController>();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddHttpClient(nameof(ChessController), c => c.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"));
-builder.Services.Configure<NeuralConfig>(builder.Configuration.GetSection(nameof(NeuralConfig)));
-builder.Services.Configure<SmtpConfig>(builder.Configuration.GetSection(nameof(SmtpConfig)));
-
-
-var neuralConfig = builder.Configuration.GetSection(nameof(NeuralConfig)).Get<NeuralConfig>();
-
-string connectionString = $"Data Source={neuralConfig.BrainFileName}";
-builder.Services.AddDbContext<ChessDbContext>(options => options.UseSqlite(connectionString));
-builder.Services.AddHostedService<Worker>();
-
-
-builder.Services.AddScoped<NeuralNetwork>(provider =>
+public partial class Program
 {
-    List<Node> nodes = new()
+    public static void Main(string[] args)
     {
-        new Node()
-        {
-            Activation = new LeakyRelu(),
-            NbHiddenNode = 69, // number of cases on the chessboard
-        },
-        new Node()
-        {
-            Activation = new LeakyRelu(),
-            NbHiddenNode = 60
-        },
-        new Node()
-        {
-            Activation = new Sigmoid(),
-            NbHiddenNode = 60
-        },
-        new Node()
-        {
-            Activation = new LeakyRelu(),
-            NbHiddenNode = 60
-        },
-        new Node()
-        {
-            Activation = new LeakyRelu(),
-            NbHiddenNode = 60
-        },
-        new Node()
-        {
-            Activation = new Sigmoid(),
-            NbHiddenNode = 60
-        },
-        new Node()
-        {
-            Activation = new Sigmoid(),
-            NbHiddenNode = 1
-        }
-    };
+        BuildWebHost(args).Run();
+    }
 
-    var neuralConfig = provider.GetRequiredService<IOptions<NeuralConfig>>();
-    var logger = provider.GetRequiredService<ILogger<NeuralNetwork>>();
-    var learningRate = neuralConfig.Value.LearningRate;
-    var chessDbContext = provider.GetRequiredService<ChessDbContext>();
-    var nbInputNodes = neuralConfig.Value.NumberOfInputNeurons;
-    return new NeuralNetwork(nbInputNodes, learningRate, nodes.ToArray(), chessDbContext, logger);
-});
-
-var app = builder.Build();
-
-    app.UseSwagger();
-    app.UseSwaggerUI();
-app.UseRewriter(new RewriteOptions().AddRedirect("^$", "swagger"));
-
-using (var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
-{
-    var dbContext = serviceScope.ServiceProvider.GetRequiredService<ChessDbContext>();
-    dbContext.Database.Migrate();
+    public static IWebHost BuildWebHost(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ AI Chess/Startup.cs
@@ -0,0 +1,105 @@
+using AI_Chess;
+using AI_Chess.Activation;
+using AI_Chess.Context;
+using AI_Chess.Controllers;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+ 
+    public IConfiguration Configuration { get; }
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddScoped<ChessController>();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddHttpClient(nameof(ChessController), c => c.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"));
+        services.Configure<NeuralConfig>(Configuration.GetSection(nameof(NeuralConfig)));
+        services.Configure<SmtpConfig>(Configuration.GetSection(nameof(SmtpConfig)));
+
+
+        var neuralConfig = Configuration.GetSection(nameof(NeuralConfig)).Get<NeuralConfig>();
+
+        string connectionString = $"Data Source={neuralConfig.BrainFileName}";
+        services.AddDbContext<ChessDbContext>(options => options.UseSqlite(connectionString));
+        services.AddHostedService<Worker>();
+
+
+        services.AddScoped<NeuralNetwork>(provider =>
+        {
+        List<Node> nodes = new()
+        {
+            new Node()
+            {
+                Activation = new LeakyRelu(),
+                NbHiddenNode = 69, // number of cases on the chessboard
+            },
+            new Node()
+            {
+                Activation = new LeakyRelu(),
+                NbHiddenNode = 60
+            },
+            new Node()
+            {
+                Activation = new Sigmoid(),
+                NbHiddenNode = 60
+            },
+            new Node()
+            {
+                Activation = new LeakyRelu(),
+                NbHiddenNode = 60
+            },
+            new Node()
+            {
+                Activation = new LeakyRelu(),
+                NbHiddenNode = 60
+            },
+            new Node()
+            {
+                Activation = new Sigmoid(),
+                NbHiddenNode = 60
+            },
+            new Node()
+            {
+                Activation = new Sigmoid(),
+                NbHiddenNode = 1
+            }
+        };
+
+        var neuralConfig = provider.GetRequiredService<IOptions<NeuralConfig>>();
+        var logger = provider.GetRequiredService<ILogger<NeuralNetwork>>();
+        var learningRate = neuralConfig.Value.LearningRate;
+        var chessDbContext = provider.GetRequiredService<ChessDbContext>();
+        var nbInputNodes = neuralConfig.Value.NumberOfInputNeurons;
+        return new NeuralNetwork(nbInputNodes, learningRate, nodes.ToArray(), chessDbContext, logger);
+        });
+
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.UseRewriter(new RewriteOptions().AddRedirect("^$", "swagger"));
+
+        using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+        {
+        var dbContext = serviceScope.ServiceProvider.GetRequiredService<ChessDbContext>();
+        dbContext.Database.Migrate();
+        }
+
+        app.UseHttpsRedirection();
+        
+        app.UseRouting();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
+}
