ddeea78f5d2ec55fd43b37726080f0b69de28dc5
BlastChaos <76956740+BlastChaos@users.noreply.github.com>
2024-04-30 17:48:46 -0400

    ajoute key attribut

--- AI Chess/Model/BContent.cs
+++ AI Chess/Model/BContent.cs
@@ -1,7 +1,10 @@
-﻿namespace AI_Chess.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AI_Chess.Model
 {
     public class BContent
     {
+        [Key]
         public int Id { get; set; }
         public int Position { get; set; }
         public double[] Value { get; set; }
